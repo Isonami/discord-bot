99d1a5bb0c4811953c93b501da5b29664241c7fe
Isonami <levru333@gmail.com>
2015-09-29 14:29:57 +0300

    no text error

--- modules/wolframalpha.py
+++ modules/wolframalpha.py
@@ -166,7 +166,7 @@ def main(self, message, *args, **kwargs):
                     if len(text_node.childNodes) == 0 and len(img_node) == 0 :
                         continue
                     text = ""
-                    if len(img_node) > 0:
+                    if len(text_node.childNodes) > 0:
                         text = text_node.childNodes[0].data
                     if len(img_node) > 0:
                         img_src = img_node[0].getAttribute('src')
