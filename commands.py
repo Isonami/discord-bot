d0d77f3a98984042c94f205509fe05039a940e9f
Isonami <levru333@gmail.com>
2015-09-29 14:58:37 +0300

    admins

--- commands.py
+++ commands.py
@@ -1,6 +1,6 @@
 commands = [
     (r"h(?:elp)?", "help", "{cmd_start}h(elp) - show help"),
     (r"\$(?P<currency>(?: [a-z]{3})+)?", "exchangerates", "{cmd_start}$ USD|EUR - show exchange rates"),
-    (r"w(?:alpha)? (?P<question>[ a-z0-9\+\?\^]{1,255})(?P<clear> forgot)?", "wolframalpha",
+    (r"w(?:alpha)? (?P<clear>forgot )?(?P<question>[ a-z0-9\+\?\^]{1,255})", "wolframalpha",
      "{cmd_start}w(alpha) - question (1-255 symbols)"),
 ]
