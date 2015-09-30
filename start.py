a98b6ec6de6c1668e49c64123671a47857dd914f
Isonami <levru333@gmail.com>
2015-09-30 13:48:14 +0300

    update start and wolfram unicode

--- start.py
+++ start.py
@@ -7,26 +7,26 @@ import bot
 
 
 class MyDaemon(Daemon):
-        def run(self):
-            sctl_daemon = threading.Thread(name='BotProccess', target=bot.main)
-            sctl_daemon.start()
-            while True:
-                sleep(1)
+    def run(self):
+        sctl_daemon = threading.Thread(name='BotProccess', target=bot.main)
+        sctl_daemon.start()
+        while True:
+            sleep(1)
 
 
 if __name__ == "__main__":
-        daemon = MyDaemon(bot.PID)
-        if len(sys.argv) == 2 or len(sys.argv) == 3:
-                if 'start' == sys.argv[1]:
-                        daemon.start()
-                elif 'stop' == sys.argv[1]:
-                        daemon.stop()
-                elif 'restart' == sys.argv[1]:
-                        daemon.restart()
-                else:
-                        print "Unknown command"
-                        sys.exit(2)
-                sys.exit(0)
+    daemon = MyDaemon(bot.PID)
+    if len(sys.argv) == 2 or len(sys.argv) == 3:
+        if 'start' == sys.argv[1]:
+            daemon.start()
+        elif 'stop' == sys.argv[1]:
+            daemon.stop()
+        elif 'restart' == sys.argv[1]:
+            daemon.restart()
         else:
-                print "usage: %s start|stop|restart" % sys.argv[0]
-                sys.exit(2)
+            print "Unknown command"
+            sys.exit(2)
+        sys.exit(0)
+    else:
+        print "usage: %s start|stop|restart" % sys.argv[0]
+        sys.exit(2)
