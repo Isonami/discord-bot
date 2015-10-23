380ed69dfa6dc42fe481b3d9b357639a88f458ad
Isonami <levru333@gmail.com>
2015-10-23 15:43:40 +0300

    restart fix

--- modules/restart.py
+++ modules/restart.py
@@ -1,6 +1,7 @@
 import logging
 import os
 import sys
+from threading import Thread
 logger = logging.getLogger(__name__)
 command = "%s {maindir}/start.py restart" % sys.executable
 
@@ -11,13 +12,18 @@ def init(bot):
     command = command.format(maindir=bot.config.get("main.dir"))
 
 
+def restart():
+    pid = os.fork()
+    if pid == 0:
+        os.system("nohup %s &" % sys.executable)
+        exit()
+
+
 def main(self, message, *args, **kwargs):
     try:
         if self.is_admin(message.author) and message.channel.is_private:
             self.send(message.channel, "Restarting...")
-            pid = os.fork()
-            if pid == 0:
-                os.system("nohup %s &" % sys.executable)
-                exit()
+            th = Thread(target=restart)
+            th.start()
     except Exception, exc:
         logger.error("%s: %s" % (exc.__class__.__name__, exc))
