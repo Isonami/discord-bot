db91af25587fd0995bd96bc4d48e7922343e17ed
Isonami <levru333@gmail.com>
2015-10-29 15:52:23 +0300

    version

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os.path as path
 import logging
 import json
 from datetime import datetime
-VERSION = "2.0.7"
+VERSION = "2.0.8"
 PID = "/var/run/discord-bot/bot.pid"
 CONFIG = {
     "discord": {
