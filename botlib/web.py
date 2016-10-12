e9fb2ef166261d2a5cd8379e20a460417cae51ab
Isonami <levru333@gmail.com>
2016-10-12 19:25:00 +0300

    asyncio update

--- botlib/web.py
+++ botlib/web.py
@@ -81,8 +81,8 @@ class TopicHandler(tornado.web.RequestHandler):
         super().__init__(*args)
 
     @deco
-    async def update(hannel, topic='')
-        await self.bot.edit_channel(channel, topic=msg)
+    async def upd(self, channel, topic=''):
+        await self.bot.edit_channel(channel, topic=topic)
 
     async def post(self, api, server, channel):
         self.set_header("Content-Type", "application/json; charset=UTF-8")
@@ -98,7 +98,7 @@ class TopicHandler(tornado.web.RequestHandler):
         except UnicodeDecodeError:
             self.write('{"status":"502", "text":"can not decode message"}')
             return
-        await self.update(channel, topic=msg)
+        await self.upd(channel, topic=msg)
         self.write('{"status":"200"}')
 
 
