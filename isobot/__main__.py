cd310d38de52bbf5b0d8f20306e14ce50acc8b04
Isonami <levru333@gmail.com>
2017-11-30 17:27:32 +0300

    fix module execution

---
+++ isobot/__main__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4 syntax=python
+# vi: tabstop=8 expandtab shiftwidth=4 softtabstop=4 syntax=python
+from . import main
+
+if __name__ == '__main__':
+    main()
